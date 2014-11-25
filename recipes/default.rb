34cd82579681ded4a193728c32752612051f004d
Anton Panasenko <anton.panasenko@gmail.com>
2014-11-24 16:31:38 -0800

    Fixed path to home directory

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,13 @@
+if ( node['qubell-nodejs']['app_root'].start_with?('~') )
+  node.set['qubell-nodejs']['app_root'] = node['qubell-nodejs']['app_root'].gsub('~', ENV['HOME'])
+end
+
 include_recipe "qubell-nodejs::nodejs_install"
 include_recipe "qubell-nodejs::npm_install"
 include_recipe "qubell-nodejs::npm_install_local"
 include_recipe "qubell-nodejs::prestart_commands"
 include_recipe "qubell-nodejs::start_commands"
 
-if ( node['qubell-nodejs']['app_root'].start_with?('~') )
-  node.set['qubell-nodejs']['app_root'] = node['qubell-nodejs']['app_root'].gsub('~', ENV['HOME'])
-end
 case node["platform_family"]
   when "rhel"
     service "iptables" do
